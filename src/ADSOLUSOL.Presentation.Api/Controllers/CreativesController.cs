ba0a587a28d98330b383a3032b40c71a534e79ce
papassito <info@solusol.net>
2026-09-17 14:39:48 -0700

    020

--- src/ADSOLUSOL.Presentation.Api/Controllers/CreativesController.cs
+++ src/ADSOLUSOL.Presentation.Api/Controllers/CreativesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ADSOLUSOL.Presentation.Api.Controllers;
 
 [ApiController]
-[Route("api/creatives")]
+[Route("api/[controller]")]
 public class CreativesController : ControllerBase
 {
     private readonly ICreativeRepository _creativeRepository;
@@ -18,8 +18,8 @@ public class CreativesController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> CreateCreative([FromBody] Creative creative)
     {
-        creative.CreatedAtUtc = DateTime.UtcNow;
-        creative.UpdatedAtUtc = DateTime.UtcNow;
+        creative.CreatedAt = DateTime.UtcNow;
+        creative.UpdatedAt = DateTime.UtcNow;
         var id = await _creativeRepository.CreateAsync(creative);
         var createdCreative = await _creativeRepository.GetByIdAsync(id);
         return CreatedAtAction(nameof(CreateCreative), new { id }, createdCreative);
