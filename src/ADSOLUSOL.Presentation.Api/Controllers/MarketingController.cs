dc6928ce36f57c1a8354c2e2b789b5c8c187c98d
papassito <info@solusol.net>
2026-09-17 16:12:50 -0700

    032

--- src/ADSOLUSOL.Presentation.Api/Controllers/MarketingController.cs
+++ src/ADSOLUSOL.Presentation.Api/Controllers/MarketingController.cs
@@ -1,10 +1,11 @@
-﻿﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ADSOLUSOL.Presentation.Api.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/marketing/adsolusol")]
 public class MarketingController : ControllerBase
 {
     // Endpoints to be implemented here
+    // This valid class structure resolves the CS1022 build error.
 }
