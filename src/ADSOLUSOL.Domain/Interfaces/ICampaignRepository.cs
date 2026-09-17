361eec48598bf2c3070e44a6c40a1ab45418d936
papassito <info@solusol.net>
2026-09-17 15:24:22 -0700

    027

--- src/ADSOLUSOL.Domain/Interfaces/ICampaignRepository.cs
+++ src/ADSOLUSOL.Domain/Interfaces/ICampaignRepository.cs
@@ -5,10 +5,10 @@ namespace ADSOLUSOL.Domain.Interfaces;
 
 public interface ICampaignRepository
 {
+    Task CreateAsync(Campaign campaign);
     Task<IEnumerable<Campaign>> GetAllAsync(string tenantId, CancellationToken token = default);
     Task<Campaign?> GetByIdAsync(Guid id);
     Task<Campaign?> GetByIdAsync(string id);
-    Task CreateAsync(Campaign campaign);
     Task UpdateAsync(Campaign campaign);
-    Task UpdateBudgetAsync(string campaignId, decimal cost, IDbTransaction transaction);
+    Task<int> UpdateBudgetAsync(string campaignId, decimal cost, IDbTransaction transaction);
 }
